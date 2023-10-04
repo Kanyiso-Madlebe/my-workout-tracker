4ee68790a7edc9c82caf6fdfe989f2cdcb830500
Kanyiso Madlebe <TestEmail@mail.com>
2023-10-04 09:01:42 +0200

    Changes to be committed: 	modified:   src/components/pages/pages/Login.jsx

--- src/components/pages/pages/Login.jsx
+++ src/components/pages/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/login.css';
-
+   
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
