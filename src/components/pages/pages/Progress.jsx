631509b18bdd729205371d437ed0576dac0d6cfd
Kanyiso Madlebe <TestEmail@mail.com>
2023-11-02 10:14:04 +0200

    Changes to be committed: 	modified:   src/components/pages/pages/Progress.jsx 	modified:   src/components/pages/styles/progress.css

--- src/components/pages/pages/Progress.jsx
+++ src/components/pages/pages/Progress.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import '../styles/progress.css';
 
 function Progress() {
@@ -7,6 +7,15 @@ function Progress() {
     <div className="progress-container">
       <Link to="/menu" className="back-arrow">&#8592;</Link>
       <h1 className="page-heading">My Progress</h1>
+      <div className="progress-indicator">
+        <div className="arrow left">&#9664;</div>
+        <div className="progress-circle">
+          <div className="progress-circle-inner">
+            <div className="progress-circle-progress"></div>
+          </div>
+        </div>
+        <div className="arrow right">&#9654;</div>
+      </div>
       {/* Your content for the Progress page goes here */}
     </div>
   );
