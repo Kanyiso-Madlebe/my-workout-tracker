ef2aeed984ab5f19ccaf5d78d045966804773450
Kanyiso Madlebe <TestEmail@mail.com>
2023-11-07 11:39:53 +0200

    Changes to be committed: 	modified:   src/components/pages/pages/Faq.jsx

--- src/components/pages/pages/Faq.jsx
+++ src/components/pages/pages/Faq.jsx
@@ -9,34 +9,29 @@ function FAQ() {
       question: "What is DGC?",
       answer: "DGC stands for 'Daily Gym Companion,' and it's a workout tracker app designed to help you monitor and improve your fitness journey."
     },
-    {
-      id: 2,
-      question: "How can I download DGC?",
-      answer: "You can download DGC from the App Store for iOS devices or Google Play Store for Android devices. Simply search for 'DGC' and install it."
-    },
-    {
-      id: 3,
-      question: "Is DGC free to use?",
-      answer: "Yes, DGC offers a free version with basic features."
-    },
-    {
-      id: 4,
-      question: "How do I create a workout plan?",
-      answer: "To create a workout plan, go to the 'Workouts' section in the app, click on 'Create New Plan,' and follow the on-screen instructions. You can customize your exercises, sets, and reps."
-    },
-    {
-      id: 5,
-      question: "Can I track my progress with DGC?",
-      answer: "Absolutely! DGC allows you to track your daily step count, calories burned, and workout progress. You can set fitness goals and monitor your achievements."
-    },
+    // Add more initial questions and answers here
   ]);
+
+  const [newQuestion, setNewQuestion] = useState('');
+
+  const handleQuestionSubmit = () => {
+    // Send the new question to the admin portal (you'll need to implement this part)
+    // For now, we'll just update the state with the new question
+    setQuestions([
+      ...questions,
+      {
+        id: questions.length + 1,
+        question: newQuestion,
+        answer: "This question is awaiting an answer from the admin."
+      }
+    ]);
+    setNewQuestion(''); // Clear the input field
+  };
 
   return (
     <div className="faq-page">
-        <Link to="/menu" className="back">
-        &#8592; 
-      </Link>
       <h1>Frequently Asked Questions (FAQ)</h1>
+
       <ul className="question-list">
         {questions.map((q) => (
           <li key={q.id}>
@@ -45,6 +40,16 @@ function FAQ() {
           </li>
         ))}
       </ul>
+
+      <div className="user-input">
+        <input
+          type="text"
+          value={newQuestion}
+          placeholder="Ask a question..."
+          onChange={(e) => setNewQuestion(e.target.value)}
+        />
+        <button onClick={handleQuestionSubmit}>Submit</button>
+      </div>
     </div>
   );
 }
